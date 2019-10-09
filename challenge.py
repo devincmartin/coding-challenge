c1e6c3e0749deac71939f0450c8437175e14f77c
Devin Martin <41971481+devincmartin@users.noreply.github.com>
2019-10-09 01:02:24 -0400

    Create challenge.py

---
+++ challenge.py
@@ -0,0 +1,48 @@
+#Coding Challenge Many Matches test
+# test your code
+y = input("What is the famous tune about peper piper? ")
+x = input("What is the sixteenth letter in the alphabet?")
+if x == "p":
+    print("9 ")
+    if s == "peter piper picked a pair of pickled peppers p":
+      print(s.replace('p', ''))
+
+
+# Coding Challenge No Substring Test
+# test your code
+x = input("What do you call a group of players forming one side in a competitive game or sport? ")
+
+y = input("There is no what in team? ")
+
+if x == "team":
+  print("0")
+  if y == "i":
+    print("team")
+
+
+# Coding Challenge One Substring Test
+# test your code
+x = input("What is the first and last name of the previous owner of Microsoft? ")
+
+y = input("What is the last name of the the previous owner of Microsoft? ")
+
+if x == "Bill Gates":
+  print("1")
+  if y == "Gates":
+    print("Bill s")
+
+# Coding Challenge Starts With Substring Test
+# test your code
+x = input("What is the first and last name of the lead actor who played in the award-winning movie The Notebook? ")
+
+y = input("What is the first name of the lead actor who played in the award-winning movie The Notebook? ")
+
+if x == "Ryan Gosling":
+  print("1")
+  if y == "Ryan":
+    print("Gosling")
+
+
+
+# We'll go over input next week, but this is how we will
+# test your code.
